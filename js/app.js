f73d15ca539ad7bbd884d5f15398c981af44c5f9
Paulina <95p.kowalczyk@gmail.com>
2017-03-06 19:36:53 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $("button").click(function() {
+    $(".one").fadeToggle();
+    $(".two").fadeToggle();
+    $(".three").fadeToggle();
+    $(".four").fadeToggle();
+    $(".five").fadeToggle();
+    $(".six").fadeToggle();
+    
+  });
+};
+
+$(document).ready(main);
